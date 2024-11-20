c16d95f6ac7ebccc32a9ce6423299412852dd1e8
mfehlman1 <161093831+mfehlman1@users.noreply.github.com>
2024-11-19 18:43:32 -0800

    Update controllers.py

--- apps/topical/controllers.py
+++ apps/topical/controllers.py
@@ -7,7 +7,8 @@ from .models import db, parse_post_content, get_user_email
 @action.uses("index.html", auth)
 def index():
     user_id = auth.current_user.get("id") if auth.current_user else None
-    print("Backend Debug: Logged-in User ID:", user_id)  
+    print("Auth Current User:", auth.current_user) 
+    print("Backend Debug: user_id:", user_id)  
     return {"user_id": user_id}
 
 @action('create_post', method=['POST'])
